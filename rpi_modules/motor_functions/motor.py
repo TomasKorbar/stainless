e78d0c6fae413b4e153af0e688adb2445f9e0f79
Tomas Korbar <tkorbar@redhat.com>
2018-11-23 12:24:49 +0100

    Motor class development

--- rpi_modules/motor_functions/motor.py
+++ rpi_modules/motor_functions/motor.py
@@ -1,4 +1,29 @@
+import time
 import gpiod
 
+from stainless.rpi_modules import consts
+
 class Motor():
-	pass
+	def __init__(self, pins):
+		self._chip = gpiod.Chip("gpiochip0")
+		self._lines = self._chip.get_lines(pins)
+		self._lines.request(consumer="gpiochip0",
+			type=gpiod.LINE_REQ_DIR_OUT
+		)
+		self._current_step = 0
+
+	def turn(self, angle):
+		target_angle = angle
+		next_step = 1 if target_angle > 0 else 0
+		while target_angle > consts.HALFSTEP_ANGLE or target_angle < -consts.HALFSTEP_ANGLE:
+			self._lines.set_values(consts.HALFSTEP_SEQUENCE[self._current_step])
+			self._current_step += next_step
+			if self._current_step > 7:
+				self._current_step = 0
+			else if self._current_step < 0:
+				self._current_step = 7
+			time.sleep(0.001)
+
+if __name__ == "__main__":
+	m = Motor([2,3,4,17])
+	m.turn(90)
