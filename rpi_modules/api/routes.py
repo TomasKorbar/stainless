24e59136b6146b31b94c57a5a64ce80110089124
Tomas Korbar <tkorbar@redhat.com>
2018-11-23 16:39:43 +0100

    api dev

--- rpi_modules/api/routes.py
+++ rpi_modules/api/routes.py
@@ -1,14 +1,20 @@
+import stainless.rpi_modules.motor_functions.robot_controll as rbcontroll
 from flask import Flask, Blueprint, Request, request
 
 routes = Blueprint('routes', __name__)
 
 @routes.route('/move/forward', methods=["GET"])
 def move_forward():
-	return str(request.args.get('cm'))
+	rbcontroll.move_forward(int(request.args.get("cm")))
+	return str("1")
 
-@routes.route('/move/turn')
+@routes.route('/move/turn', methods=["GET"])
 def turn():
-	return '1'
+	if int(request.args.get("angle")) > 0:
+		rbcontroll.turn_right(int(request.args.get("angle")))
+	else:
+		rbcontroll.turn_left(-1 * int(request.args.get("angle")))
+	return "1"
 
 @routes.route('/trash/isinrange')
 def isinrange():
