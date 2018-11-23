48b63ebfedc14c9acbd9917d54d498efd1799716
Tomas Korbar <tkorbar@redhat.com>
2018-11-23 16:10:56 +0100

    implement threads

--- rpi_modules/motor_functions/robot_controll.py
+++ rpi_modules/motor_functions/robot_controll.py
@@ -1,4 +1,5 @@
 from stainless.rpi_modules.motor_functions.motor import Motor
+from threading import Thread
 
 motor_left = Motor([19,13,6,5])
 motor_right = Motor([21,16,20,26])
@@ -7,8 +8,12 @@ arm_higher = Motor([24,25,1,12])
 
 def move_forward(distance):
 	while distance > 0:
-		motor_right.turn(1000)
-		motor_left.turn(-1000)
+		t1 = Thread(target=lambda: motor_right.turn(100))
+		t2 = Thread(target=lambda: motor_left.turn(-100))
+		t1.start()
+		t2.start()
+		t1.join()
+		t2.join()
 		distance -= 1
 
 def turn_left(angle):
