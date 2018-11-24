30da2a6791cf0ee9fa3a4cc435be42327d0a247a
Tomas Korbar <tkorbar@redhat.com>
2018-11-24 01:00:18 +0100

    Fix motors

--- rpi_modules/motor_functions/motor.py
+++ rpi_modules/motor_functions/motor.py
@@ -5,16 +5,17 @@ import stainless.rpi_modules.motor_functions.consts as consts
 
 class Motor():
     def __init__(self, pins):
-        self._chip = gpiod.Chip("gpiochip0")
-        self._lines = self._chip.get_lines(pins)
-        self._lines.request(consumer="gpiochip0",
-            type=gpiod.LINE_REQ_DIR_OUT
-        )
+        self._pins = pins
         self._current_step = 0
 
     def turn(self, angle):
         target_angle = angle
         next_step = -1 if target_angle > 0 else 1
+        self._chip = gpiod.Chip("gpiochip0")
+        self._lines = self._chip.get_lines(self._pins)
+        self._lines.request(consumer="gpiochip0",
+            type=gpiod.LINE_REQ_DIR_OUT
+        )
         while target_angle > consts.HALFSTEP_ANGLE or target_angle < -consts.HALFSTEP_ANGLE:
             self._lines.set_values(consts.HALFSTEP_SEQUENCE[self._current_step])
             self._current_step += next_step
@@ -24,10 +25,8 @@ class Motor():
                 self._current_step = 7
             target_angle += next_step * consts.HALFSTEP_ANGLE
             time.sleep(0.001)
-
-    def __exit__(self, exception_type, exception_value, traceback):
-        self._lines.set_values([0,0,0,0])
         self._chip.close()
+
 
 if __name__ == "__main__":
     m = Motor([2,3,4,17])
